cbbce3faf02ffa49af6e31d256daec0aa862f274
MohanKrishna <www.krishna225@gmail.com>
2020-04-14 19:19:35 +0530

    Update mango_tree.cpp

--- Face/mango_tree.cpp
+++ Face/mango_tree.cpp
@@ -1,40 +1,43 @@
-Mango tree - I
-Dora is so much interested in gardening and hence she plants more trees in her garden. She plants trees in a rectangular fashion with the order of rows and columns and numbers the trees in a row-wise order. She planted the mango tree only in the 1st row, 1st column and last column. So, given the tree number, your task is to find out whether the given tree is a mango tree or not? Now, write a program to check whether the given number denotes a mango tree or not.
-INPUT FORMAT:
-
-Input consists of 3 integers.
-
-The first input denotes the number of rows.
-
-The second input denotes the number of columns.
-
-The third input denotes the tree number.
-
-OUTPUT FORMAT:
-
-If the given number is a mango tree, print "Yes". Otherwise, print "No"
-
-Refer the sample output for formatting.
-
-SAMPLE INPUT:
-
-5
-
-5
-
-11
-
-SAMPLE OUTPUT:
-
-Yes
-
-Case 1
-Case 2
-Case 3
-Input (stdin)
-5
-5
-11
-
-Output (stdout)
-Yes
+#include<iostream>
+using namespace std;
+int main()
+{
+	int m,n,i,j,Count=0,find,k=0;
+	cin>>m>>n>>find;
+	int a[m*n];
+	for(i=0;i<m;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			Count++;
+			if(i==0 || i==(m)|| j==0)
+			{
+				a[k++]=Count;
+		     	
+			}
+		     	
+		}
+	}
+	    int flag =0;
+		for(i=0;i<m+n;i++)
+		{
+		//	cout<<" "<<a[i];
+		  if(find==a[i])
+	     	{
+		     	flag = 1;
+	     	}
+	   
+		}
+		
+		if(flag==1)
+		{
+			cout<<"Yes";	
+		}
+		else
+		{
+			cout<<"No";
+	    }
+		
+	
+	return 0;
+}
